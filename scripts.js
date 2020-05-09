dbb9b87b7055e808b8313ed4ef90899fb2093b9e
giuliaGuerreiro <giulia.guerreiro@gmail.com>
2020-05-09 11:12:40 -0300

    script added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener("click", function () {
+        const imageId = card.getAttribute('id');
+        modalOverlay.classList.add('active');
+        document.querySelector('iframe').src=`./assets/${imageId}`
+    })
+
+}
+
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove('active');
+    document.querySelector('iframe').src="";
+})
