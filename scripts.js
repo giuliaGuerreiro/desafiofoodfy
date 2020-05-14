6a7479cb0fcfd309c854296038354a093648146c
giuliaGuerreiro <giulia.guerreiro@gmail.com>
2020-05-14 16:04:13 -0300

    update before back-end

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
+const infoModal = document.querySelector('.info-modal');
+
 
 for (let card of cards) {
     card.addEventListener("click", function () {
         const imageId = card.getAttribute('id');
         modalOverlay.classList.add('active');
-        document.querySelector('iframe').src=`./assets/${imageId}`
+        document.querySelector('iframe').src=`./assets/${imageId}`;
     })
 
 }
